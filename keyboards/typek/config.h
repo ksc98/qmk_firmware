366baee827a1cc230ab618c14499b0c74df6f16a
Kyle <kyle@samaya.ai>
2025-06-14 22:34:20 -0700

    typek

--- keyboards/typek/config.h
+++ keyboards/typek/config.h
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define EECONFIG_KB_DATA_SIZE (3*6)
 
+#define HOLD_ON_OTHER_KEY_PRESS
 
 /* LED MAP
 		0 , 1 , 2														Three indicators
